0a6ec458d7ebece578a04e24db83a2d1d398b3d0
fisgix <111667497+fisgix@users.noreply.github.com>
2024-10-14 18:04:40 -0500

    Drive-Test_robot-config.cpp

--- Drive-Test_robot-config.cpp
+++ Drive-Test_robot-config.cpp
@@ -9,13 +9,13 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor leftMotorA = motor(PORT1, ratio18_1, false);
-motor leftMotorB = motor(PORT2, ratio18_1, false);
-motor leftMotorC = motor(PORT3, ratio18_1, true);
+motor leftMotorA = motor(PORT1, ratio6_1, false);
+motor leftMotorB = motor(PORT2, ratio6_1, false);
+motor leftMotorC = motor(PORT3, ratio6_1, true);
 motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT4, ratio18_1, true);
-motor rightMotorB = motor(PORT5, ratio18_1, true);
-motor rightMotorC = motor(PORT6, ratio18_1, false);
+motor rightMotorA = motor(PORT4, ratio6_1, true);
+motor rightMotorB = motor(PORT5, ratio6_1, true);
+motor rightMotorC = motor(PORT6, ratio6_1, false);
 motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
 drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
 
